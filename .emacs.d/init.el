00174a8c9cc1bf8dabb42dfdeb3ee8b35386ac40
Lukasz Byczynski <>
2018-03-22 11:18:36 +0100

    Dev dump

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,6 +16,7 @@
   (setq
   package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                      ("org" . "http://orgmode.org/elpa/")
+                     ("marmalade" . "http://marmalade-repo.org/packages/")
                      ("melpa-stable" . "http://stable.melpa.org/packages/")
                      ("marmalade" . "http://marmalade-repo.org/packages/")
                      ("melpa" . "http://melpa.org/packages/"))
