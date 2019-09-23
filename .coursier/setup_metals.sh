d0cd6b54024dafda625393ad997cd5de45b08064
Lukasz Byczynski <mail@lukaszbyczynski.com>
2019-09-23 20:25:42 +0200

    Bump metals

--- .coursier/setup_metals.sh
+++ .coursier/setup_metals.sh
@@ -4,7 +4,7 @@ rm /usr/local/bin/metals-sublime
   --java-opt -Xss4m \
   --java-opt -Xms100m \
   --java-opt -Dmetals.client=sublime \
-  org.scalameta:metals_2.12:0.7.5 \
+  org.scalameta:metals_2.12:0.7.6 \
   -r bintray:scalacenter/releases \
   -r sonatype:snapshots \
   -o /usr/local/bin/metals-sublime -f
