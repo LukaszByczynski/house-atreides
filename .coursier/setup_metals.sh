8c97b5cc5d484c01495ece13c28b44beb94ca55b
Lukasz Byczynski <l.byczynski@adform.com>
2020-03-25 09:01:04 +0100

    Bump sublime-metals

--- .coursier/setup_metals.sh
+++ .coursier/setup_metals.sh
@@ -4,7 +4,7 @@ rm /usr/local/bin/metals-sublime
   --java-opt -Xss4m \
   --java-opt -Xms100m \
   --java-opt -Dmetals.client=sublime \
-  org.scalameta:metals_2.12:0.7.6 \
+  org.scalameta:metals_2.12:0.8.2 \
   -r bintray:scalacenter/releases \
   -r sonatype:snapshots \
   -o /usr/local/bin/metals-sublime -f
