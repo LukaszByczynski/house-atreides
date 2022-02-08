180ecc4b1580de996fbd8b9181dfe61204f66ac0
Lukasz Byczynski <mail@lukaszbyczynski.com>
2022-02-08 20:29:17 +0100

    Add some zsh eye candy

---
+++ .zsh.d/00_zsh.zsh
@@ -0,0 +1,2 @@
+[ -f /home/linuxbrew/.linuxbrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source /home/linuxbrew/.linuxbrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+[ -f /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source /home/linuxbrew/.linuxbrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
