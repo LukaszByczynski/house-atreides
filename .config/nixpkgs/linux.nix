9e34c831c46ffd805b7deb97f34150d934fcebfb
Lukasz Byczynski <mail@lukaszbyczynski.com>
2023-04-13 14:50:59 +0200

    Bump flake and fix nv settings

--- .config/nixpkgs/linux.nix
+++ .config/nixpkgs/linux.nix
@@ -8,7 +8,7 @@ let
   # todo: this driver should be taken from unstabkle
   customNvidiaX11 = pkgs-unstable.linuxPackages.nvidia_x11_production.override {
     libsOnly = true;
-    disable32Bit = true;
+    disable32Bit = false;
   };
 
 in
@@ -29,7 +29,7 @@ in
       LIBVA_DRIVER_NAME = "nvidia";
       NVD_BACKEND = "direct";
       MOZ_DISABLE_RDD_SANDBOX = "1";
-      LD_LIBRARY_PATH="${customNvidiaX11}/lib:$LD_LIBRARY_PATH";
+      #LD_LIBRARY_PATH="${customNvidiaX11}/lib:$LD_LIBRARY_PATH";
     };
   };
 
