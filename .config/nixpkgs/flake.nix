1dc6191b1a25eb9a355bb5420395100d021d7a87
Lukasz Byczynski <mail@lukaszbyczynski.com>
2023-06-22 15:11:02 +0200

    Bump flake and fix home manager version

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
