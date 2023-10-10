98ab59749924993be8004e73a1078d899afc4355
Lukasz Byczynski <mail@lukaszbyczynski.com>
2023-10-10 10:44:57 +0200

    Comment out wrk2 (not supported on mac)

--- .config/nixpkgs/programs/default.nix
+++ .config/nixpkgs/programs/default.nix
@@ -22,7 +22,7 @@ let
       nix-index # for searching files in the packages
       kubectl   # k8s cli tool
       stress    # cpu stress tool
-      wrk2      # perf test
+      #wrk2      # perf test
       ctop      # monitoring docker containers
     ];
 
