5d39b64dd763cebade6f72367aac626612619343
Lukasz Byczynski <mail@lukaszbyczynski.com>
2024-01-09 18:47:30 +0100

    Add dive and gitui

--- .config/nixpkgs/programs-linux/default.nix
+++ .config/nixpkgs/programs-linux/default.nix
@@ -4,6 +4,8 @@ let
     home.packages = with pkgs; [
       wrk2      # perf test
       nix-du    # track disk usage by nix roots
+      dive      # A tool for exploring a docker image
+      gitui     # GitUI provides you with the comfort of a git GUI but right in your terminal
     ];
   };
 in [
