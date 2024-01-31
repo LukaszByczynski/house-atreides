2a9b1d1607696e73ac67abb8e98ab1192536a4cd
Lukasz Byczynski <mail@lukaszbyczynski.com>
2024-01-31 21:00:45 +0100

    Add mbair config

---
+++ .config/nixpkgs/osx-mbair.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, pgks-unstable, stdenv, ... }:
+
+let
+  username = "lukaszb";
+  homeDirectory = "/Users/${username}";
+  configHome = "${homeDirectory}/.config";
+in
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    inherit username homeDirectory;
+    stateVersion = "23.05";
+
+    sessionVariables = {
+      # DISPLAY = ":0";
+      EDITOR = "nvim";
+    };
+  };
+
+  # restart services on change
+  systemd.user.startServices = "sd-switch";
+
+  # notifications about home-manager news
+  news.display = "silent";
+
+  imports = builtins.concatMap import [
+    ./programs
+  ];
+}
