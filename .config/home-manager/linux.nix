be00f592ebe1ce5f08916ad879f4d8e172f5aa31
Lukasz Byczynski <mail@lukaszbyczynski.com>
2025-05-03 13:48:43 +0200

    Bump NVidia driver

--- .config/home-manager/linux.nix
+++ .config/home-manager/linux.nix
@@ -5,7 +5,7 @@ let
   homeDirectory = "/home/${username}";
   configHome = "${homeDirectory}/.config";
 
-  customNvidiaX11 = pkgs-unstable.linuxPackages.nvidia_x11.override {
+  customNvidiaX11 = pkgs-unstable.linuxPackages.nvidia_x11_production.override {
     libsOnly = true;
     disable32Bit = false;
   };
