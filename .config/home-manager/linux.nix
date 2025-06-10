fd8973c30346731a3404dc2bd8f71ad1ff9e4868
Lukasz Byczynski <mail@lukaszbyczynski.com>
2025-06-10 13:49:56 +0200

    Bump Nix pkgs

--- .config/home-manager/linux.nix
+++ .config/home-manager/linux.nix
@@ -19,9 +19,9 @@ in
 
     sessionVariables = {
       # force use nvidia vaapi driver for video decoding
-      # LIBVA_DRIVER_NAME = "nvidia";
-      # NVD_BACKEND = "direct";
-      # MOZ_DISABLE_RDD_SANDBOX = "1";
+      LIBVA_DRIVER_NAME = "nvidia";
+      NVD_BACKEND = "direct";
+      MOZ_DISABLE_RDD_SANDBOX = "1";
       MOZ_ENABLE_WAYLAND=1;
       LD_LIBRARY_PATH="${customNvidiaX11}/lib:$LD_LIBRARY_PATH";
       GBM_BACKENDS_PATH="${customNvidiaX11}/lib/gbm";
