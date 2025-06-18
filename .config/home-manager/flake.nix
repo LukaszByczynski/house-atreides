7a5fa5dc11df972ebd7a797cf7882fd14a6b0588
Lukasz Byczynski <mail@lukaszbyczynski.com>
2025-06-18 12:54:02 +0200

    Bump NIX to 25.05

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     # Specify the source of Nixpkgs and Home Manager.
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
