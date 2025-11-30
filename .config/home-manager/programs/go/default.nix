e05ce2fad63f68c27b3f81e4403978401c5ad2ce
Lukasz Byczynski <mail@lukaszbyczynski.com>
2025-11-30 22:09:49 +0100

    Go config

--- .config/home-manager/programs/go/default.nix
+++ .config/home-manager/programs/go/default.nix
@@ -3,6 +3,12 @@
 let
 in
 {
+  home.packages = with pkgs; [
+    pkgs-unstable.gopls           # go language server
+    pkgs-unstable.go-tools        # go-tools (static analyzer, etc)
+    pkgs-unstable.delve           # go debugger
+  ];
+
   programs.go = {
     package = pkgs-unstable.go;
 
