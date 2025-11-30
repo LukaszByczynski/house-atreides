790a460c8b1b603cfceb49b25d996508dd215b36
Lukasz Byczynski <mail@lukaszbyczynski.com>
2025-11-30 22:09:21 +0100

    SSH config

---
+++ .config/home-manager/programs/ssh/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, pkgs-unstable, lib, ... }: {
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      serverAliveInterval = 60;
+      serverAliveCountMax = 3;
+      compression = false;
+      addKeysToAgent = "no";
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
+    includes = ["local_config"];
+  };
+
+}
