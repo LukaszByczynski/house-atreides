ef4ce0f4ebe563ffdad320a77530fb5e469488be
Lukasz Byczynski <mail@lukaszbyczynski.com>
2026-04-17 10:58:04 +0200

    Use unstable direnv

--- .config/home-manager/programs/direnv/default.nix
+++ .config/home-manager/programs/direnv/default.nix
@@ -3,6 +3,7 @@
   # a shell extension that manages your environment
   programs.direnv = {
     enable = true;
+    package = pkgs-unstable.direnv;
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
